7ec9f3ed37cea1c888ecaeda003ce37a9921b39a
Spollys <168425120+Spollys@users.noreply.github.com>
2024-04-29 23:01:39 +0300

    improve

--- WebApplication1/Models/Doctor.cs
+++ WebApplication1/Models/Doctor.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Hospital.Models;
 
@@ -6,35 +7,55 @@ public class Doctor : ICSVParser<Doctor>
 {
     [Required]
     public int Id { get; set; }
+
     [Required]
     public string Name { get; set; } = string.Empty;
 
-    [Required] public string Speciality { get; set; } = string.Empty;
+    [Required]
+    public string Speciality { get; set; } = string.Empty;
 
-    public static Doctor Parse(string s, string separator=";")
+    public static Doctor Parse(string s, string separator = ";")
     {
         var words = s.Split(new[] { separator }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-        if (words.Length != 3 || !int.TryParse(words[0], out var id))
+
+        if (words.Length != 3 || !int.TryParse(words[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out var id))
+        {
             throw new FormatException("String cannot be parsed to the instance of Doctor type");
+        }
+
         return new Doctor { Id = id, Name = words[1], Speciality = words[2] };
     }
 
-    public override string ToString() => $"Doctor(Id = {Id}, Name = {Name}, Speciality = {Speciality})";
+    public string ToString(string separator = ";")
+    {
+        return $"{Id}{separator}{Name}{separator}{Speciality}";
+    }
 
-    //public static bool TryParse(string? s, IFormatProvider? _, out Doctor? result)
-    //{
-    //    try
-    //    {
-    //        if (s is null)
-    //            throw new NullReferenceException();
-    //        result = Parse(s);
-    //    }
-    //    catch (Exception)
-    //    {
-    //        result = null;
-    //        return false;
-    //    }
+    public static bool TryParse(string? s, IFormatProvider? provider, out Doctor? result)
+    {
+        result = null;
 
-    //    return true;
-    //}
+        if (s is null)
+        {
+            return false;
+        }
+
+        try
+        {
+            var words = s.Split(new[] { separator }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length != 3 || !int.TryParse(words[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out var id))
+            {
+                throw new FormatException("String cannot be parsed to the instance of Doctor type");
+            }
+
+            result = new Doctor { Id = id, Name = words[1], Speciality = words[2] };
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
