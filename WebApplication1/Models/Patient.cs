bdf63ee2ae0829a80d79bbfa031b534839ca67eb
Spollys <168425120+Spollys@users.noreply.github.com>
2024-04-29 23:01:43 +0300

    improve

--- WebApplication1/Models/Patient.cs
+++ WebApplication1/Models/Patient.cs
@@ -1,4 +1,4 @@
-﻿namespace Hospital.Models;
+namespace Hospital.Models;
 
 public class Patient : ICSVParser<Patient>
 {
@@ -8,13 +8,36 @@ public class Patient : ICSVParser<Patient>
 
     public static Patient Parse(string? s, string separator = ";")
     {
-        var words = s?.Split(new[] { separator }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-        if (words is null || words.Length != 3 || !int.TryParse(words[0], out var id) ||
-            !DateOnly.TryParse(words[2], out var date))
+        if (TryParse(s, separator, out var patient))
+            return patient;
+        else
             throw new FormatException("String cannot be parsed to the instance of Patient type");
-        return new Patient { Id = id, Name = words[1], BirthDate = date };
     }
 
-    //public static bool TryParse(string? s, IFormatProvider? provider, out Patient? result) =>
-    //    (result = Parse(s)) is not null;
+    public static bool TryParse(string? s, string separator = ";", out Patient? patient)
+    {
+        patient = null;
+        if (string.IsNullOrWhiteSpace(s))
+            return false;
+
+        var words = s.Split(new[] { separator }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        if (words.Length != 3 || !int.TryParse(words[0], out var id) ||
+            !DateOnly.TryParseExact(words[2], "yyyy-MM-dd", null, out var date))
+            return false;
+
+        patient = new Patient { Id = id, Name = words[1], BirthDate = date };
+        return true;
+    }
+
+    public bool TryParseFromRecord(string[] record)
+    {
+        if (record.Length != 3 || !int.TryParse(record[0], out var id) ||
+            !DateOnly.TryParseExact(record[2], "yyyy-MM-dd", null, out var date))
+            return false;
+
+        Id = id;
+        Name = record[1];
+        BirthDate = date;
+        return true;
+    }
 }
