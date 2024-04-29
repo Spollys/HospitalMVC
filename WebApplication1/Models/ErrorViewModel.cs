666be78ae41a964c626b385eea22c0e1aace0d31
Spollys <168425120+Spollys@users.noreply.github.com>
2024-04-29 23:01:36 +0300

    improve

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -1,9 +1,11 @@
-namespace WebApplication1.Models
+using System;
+
+namespace YourNamespace.Models
 {
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => RequestId is not null and not empty;
     }
 }
